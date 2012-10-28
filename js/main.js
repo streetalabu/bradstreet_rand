13402ae5d909ad03aba44aef6652e625dd9273bc
streetalabu <streetalabu@gmail.com>
2012-10-28 14:49:27 -0400

    Single Expander
    Javascript added to expand and collapse one section.  Deleted old files
    for lightbox

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 
+Shadowbox.init({
+       		handleOversize: "resize",
+    	});
+    	
+$(function(){
+		$('#mySlideContent').css('display','none');
+		$('#mySlideToggler').click(function(){
+			$('#mySlideContent').slideToggle('slow');
+			$(this).toggleClass('slideSign');
+			return false;
+		});
+		$('.collapse').click(function(){
+			$('#mySlideContent').slideToggle('slow');
+			$('#mySlideToggler').toggleClass('slideSign2');
+			return false;
+		});
+	});
